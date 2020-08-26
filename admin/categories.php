f80d0e479ae26dd89b5173b5a5c60fe97863cca3
smanch3v <38432551+smanch3v@users.noreply.github.com>
2020-08-26 22:04:39 +0300

    add cat to db / added ob_start header

--- admin/categories.php
+++ admin/categories.php
@@ -21,9 +21,35 @@
                             <small>Author</small>
                         </h1>
 
+
                         <!--- Add category form -->          
                         <div class="col-xs-6">
-                        <form action="">
+
+
+                        <?php
+
+                        if(isset($_POST['submit'])){
+                            $cat_title = $_POST['cat_title'];
+                            if($cat_title == "" || empty($cat_title) || strlen($cat_title) < 3){
+                                echo "This field should not be empty or less than 3 characters!";
+                            }else{
+                                $query = "INSERT INTO categories(cat_title)";
+                                $query.= "VALUE('$cat_title')";
+                                $create_category = mysqli_query($connection, $query);
+
+                                if(!$create_category){
+                                    die('QUERY FAILED' . mysqli_error($connection));
+                                }
+                            }
+                        }
+
+
+                        ?>
+
+
+
+
+                        <form action="" method="post">
                             <label for="cat_title">Add Category</label>
                             <div class="form-group">
                                 <div class="form-group">
