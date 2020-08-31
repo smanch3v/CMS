d4cf06033ca5f9e8c660626fe15b3dd60f654fdd
smanch3v <38432551+smanch3v@users.noreply.github.com>
2020-08-31 21:17:22 +0300

    Fixed link in nav

--- category.php
+++ category.php
@@ -15,7 +15,10 @@ include "includes/header.php";
             <div class="col-md-8">
 
             <?php
-            $query = "SELECT * FROM posts";
+            if(isset($_GET['category'])){
+                $post_category_id = $_GET['category'];
+            }
+            $query = "SELECT * FROM posts WHERE post_category_id = '$post_category_id'";
             $select_all_posts_query = mysqli_query($connection,$query); 
 
             while($row = mysqli_fetch_assoc($select_all_posts_query)){
@@ -24,7 +27,7 @@ include "includes/header.php";
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
                 $post_image = $row['post_image'];
-                $post_content = substr($row['post_content'],0,300);
+                $post_content = substr($row['post_content'],0,150);
                 ?>
 
                 <h1 class="page-header">
