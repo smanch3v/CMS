6e74ee18a340bbcd8f9bfd493613e21bceabb6eb
smanch3v <38432551+smanch3v@users.noreply.github.com>
2020-09-26 23:18:27 +0300

    Added WYSIWYG editor for posts

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -39,6 +39,9 @@ if(!isset($_SESSION['user_role'])){
     <![endif]-->
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src="js/scripts.js"></script>
+    
+    <script src="https://cdn.ckeditor.com/ckeditor5/22.0.0/classic/ckeditor.js"></script>
 
 </head>
 
